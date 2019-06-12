1c5235be65137e541425a212bb799fb9dfafbecb
luis-correaB <lcob31@outlook.com>
2019-06-11 19:33:17 -0500

    lists_new

---
+++ Ejercicio7/Lists/list1/list2/listanimals/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace listanimals {
+    class Program {
+        static void Main (string[] args) {
+            var animals = new List<string> () { "gato", "perro" };
+            // Insert strings from an array in position 1.
+            animals.InsertRange (1, new string[] { "leon", "oso" });
+            animals.InsertRange (2, new string[] { "caballo", "aguila" });
+            animals.Sort();
+            
+            foreach (string value in animals) {
+                Console.WriteLine ("RESULTADO: " + value.ToUpper());
+            }
+        }
+    }
+}
