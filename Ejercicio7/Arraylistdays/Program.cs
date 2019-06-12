85958d28cc53829c92e34a9617e7d541d1f08ebe
luis-correaB <lcob31@outlook.com>
2019-06-11 19:19:35 -0500

    list examples

---
+++ Ejercicio7/Arraylistdays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections ;
+
+namespace Arraylistdays {
+    class Program {
+        static void Main (string[] args) {
+            ArrayList arr = new ArrayList ();
+            arr.Add ("domingo");
+            arr.Add ("Lunes");
+            arr.Add ("Martes");
+            arr.Add ("Miercoles");
+            arr.Add ("Jueves");
+            arr.Add ("Viernes");
+            arr.Add ("Sabado");
+            Console.WriteLine ("los elementos del ArrayList son:");
+            foreach (object dia in arr) {
+                Console.WriteLine (dia);
+                
+            }
+            Console.WriteLine(" El numero de elementos del arreglo es"+ arr.Count);
+            
+        }
+    }
+}
---
+++ Ejercicio7/Lists/list1/listprint/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace listprint {
+    class Program {
+        public static void Main (string[] args) {
+            List<int> list = new List<int> ();
+            list.Add (4);
+            list.Add (7);
+            list.Add (9);
+            list.Add (11);
+
+            // Loop through List with foreach.
+            foreach (int num in list) {
+                Console.WriteLine (num);
+            }
+        }
+    }
+}
