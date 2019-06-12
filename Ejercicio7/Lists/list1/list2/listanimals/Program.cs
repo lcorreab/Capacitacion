8b1968bd5452a6862e4365c4a162a63783161725
luis-correaB <lcob31@outlook.com>
2019-06-11 19:53:05 -0500

    list changes

--- Ejercicio7/Lists/list1/list2/listanimals/Program.cs
+++ Ejercicio7/Lists/list1/list2/listanimals/Program.cs
@@ -9,10 +9,23 @@ namespace listanimals {
             animals.InsertRange (1, new string[] { "leon", "oso" });
             animals.InsertRange (2, new string[] { "caballo", "aguila" });
             animals.Sort();
-            
+             List<string> animalscopy = new List<string>(animals); 
+            animalscopy.Add("perro labrador");
+            animalscopy.Add("gato siames");
+
             foreach (string value in animals) {
-                Console.WriteLine ("RESULTADO: " + value.ToUpper());
+                Console.WriteLine ("Animal coleccion 1 es: " + value.ToUpper());
+               
             }
+
+            foreach (string newvalue in animalscopy) {
+                Console.WriteLine ("Animal coleecion 2 es: " + newvalue.ToUpper());
+               
+            }
+
+             Console.WriteLine("el numero de objetos  de la lista 1 es " +  animals.Count);
+              Console.WriteLine("el numero de objetos  de la lista 2 es " +  animalscopy.Count);
         }
+
     }
 }
